6d348fac92d667866ee7630576832ad05a9331fc
matsarv <mats.arvidsson@outlook.com>
2019-01-23 09:56:07 +0100

    Add project files.

---
+++ ConsoleGolf/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
